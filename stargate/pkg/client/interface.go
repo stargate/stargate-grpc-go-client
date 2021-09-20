b421dcf93dbaf21d33db31418a779b5ee9f8275d
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2021-09-20 14:05:55 -0500

    Add batch support

--- stargate/pkg/client/interface.go
+++ stargate/pkg/client/interface.go
@@ -4,5 +4,5 @@ import pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
 
 type StargateClientIFace interface {
 	ExecuteQuery(query *pb.Query) (*pb.Response, error)
-	ExecuteBatch(batch *Batch) (*pb.Response, error)
+	ExecuteBatch(batch *pb.Batch) (*pb.Response, error)
 }
