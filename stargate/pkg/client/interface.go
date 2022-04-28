9f116659df54cffd289455dbc965a57464c0fc62
Spencer Connaughton <spencer@spencerconnaughton.com>
2022-04-28 11:41:00 -0400

    Rename and expand client interface.

--- stargate/pkg/client/interface.go
+++ stargate/pkg/client/interface.go
@@ -1,8 +1,22 @@
 package client
 
-import pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
+import (
+	"context"
 
-type StargateClientIFace interface {
+	pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
+)
+
+// StargateQueryExecutor represents an interface that Astra table clients can
+// use to execute queries.
+type StargateQueryExecutor interface {
 	ExecuteQuery(query *pb.Query) (*pb.Response, error)
+	ExecuteQueryWithContext(query *pb.Query, ctx context.Context) (
+		*pb.Response,
+		error,
+	)
 	ExecuteBatch(batch *pb.Batch) (*pb.Response, error)
+	ExecuteBatchWithContext(batch *pb.Batch, ctx context.Context) (
+		*pb.Response,
+		error,
+	)
 }
